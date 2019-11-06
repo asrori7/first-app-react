d193cdb463b530a6ea17a9f49492db86ccfe9dc7
Asrori <rorijoe@gmail.com>
2019-11-06 15:13:01 +0700

    make header and add boostrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,156 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
+import 'bootstrap'
+import('bootstrap/dist/css/bootstrap.css')
 
-function App() {
+/* eslint-disable */
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar navbar-expand-md fixed-top navbar-dark bg-dark">
+        <a href="#" className="navbar-brand">Rori React</a>
+        <button className="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Dashboard <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Notifications</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Profile</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Switch account</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Settings</a>
+              <div className="dropdown-menu" aria-labelledby="dropdown01">
+                <a className="dropdown-item" href="#">Action</a>
+                <a className="dropdown-item" href="#">Another action</a>
+                <a className="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+          </ul>
+          {/* <form className="form-inline my-2 my-lg-0">
+            <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form> */}
+        </div>
+      </nav>
+
+      <div className="nav-scroller bg-white box-shadow">
+        <marquee>
+          <nav className="nav nav-underline">
+            <span className="nav-link" role="img" aria-label="laugh">😄</span>
+            <span className="nav-link" role="img" aria-label="cloth">👕</span>
+            <span className="nav-link" role="img" aria-label="smile">😀</span>
+            <span className="nav-link" role="img" aria-label="sheep">🐑</span>
+            <span className="nav-link" role="img" aria-label="star">✨</span>
+            <span className="nav-link" role="img" aria-label="jempol">👍</span>
+          </nav>
+        </marquee>
+      </div>
+
+      <main role="main" className="container-fluid">
+        <div className="d-flex align-items-center p-3 my-3 text-white-50 bg-purple rounded box-shadow">
+          {/* <img className="mr-3" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-outline.svg" alt="" width="48" height="48" /> */}
+          <div className="lh-100">
+            <h6 className="mb-0 text-white lh-100">Home</h6>
+            <small>Since 2019</small>
+          </div>
+        </div>
+
+        <div className="my-3 p-3 bg-white rounded box-shadow">
+          <h6 className="border-bottom border-gray pb-2 mb-0">Recent updates</h6>
+          <div className="media text-muted pt-3">
+            <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" className="mr-2 rounded" />
+            <p className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+              <strong className="d-block text-gray-dark">@username</strong>
+              Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+            </p>
+          </div>
+          <div className="media text-muted pt-3">
+            <img data-src="holder.js/32x32?theme=thumb&bg=e83e8c&fg=e83e8c&size=1" alt="" className="mr-2 rounded" />
+            <p className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+              <strong className="d-block text-gray-dark">@username</strong>
+              Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+            </p>
+          </div>
+          <div className="media text-muted pt-3">
+            <img data-src="holder.js/32x32?theme=thumb&bg=6f42c1&fg=6f42c1&size=1" alt="" className="mr-2 rounded" />
+            <p className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+              <strong className="d-block text-gray-dark">@username</strong>
+              Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+            </p>
+          </div>
+          <small className="d-block text-right mt-3">
+            <a href="#">All updates</a>
+          </small>
+        </div>
+
+        <div className="my-3 p-3 bg-white rounded box-shadow">
+          <h6 className="border-bottom border-gray pb-2 mb-0">Suggestions</h6>
+          <div className="media text-muted pt-3">
+            <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" className="mr-2 rounded" />
+            <div className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+              <div className="d-flex justify-content-between align-items-center w-100">
+                <strong className="text-gray-dark">Full Name</strong>
+                <a href="#">Follow</a>
+              </div>
+              <span className="d-block">@username</span>
+            </div>
+          </div>
+          <div className="media text-muted pt-3">
+            <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" className="mr-2 rounded" />
+            <div className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+              <div className="d-flex justify-content-between align-items-center w-100">
+                <strong className="text-gray-dark">Full Name</strong>
+                <a href="#">Follow</a>
+              </div>
+              <span className="d-block">@username</span>
+            </div>
+          </div>
+          <div className="media text-muted pt-3">
+            <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" className="mr-2 rounded" />
+            <div className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+              <div className="d-flex justify-content-between align-items-center w-100">
+                <strong className="text-gray-dark">Full Name</strong>
+                <a href="#">Follow</a>
+              </div>
+              <span className="d-block">@username</span>
+            </div>
+          </div>
+          <small className="d-block text-right mt-3">
+            <a href="#">All suggestions</a>
+          </small>
+        </div>
+      </main>
     </div>
   );
 }
+
+// function Home() {
+//   return <h2>Home</h2>;
+// }
+
+// function About() {
+//   return <h2>About</h2>;
+// }
+
+// function Users() {
+//   return <h2>Users</h2>;
+// }
 
 export default App;
