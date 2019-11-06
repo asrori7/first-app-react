68baa0c85ff0537179941bb43eb272ffb1dae9c4
Asrori <rorijoe@gmail.com>
2019-11-06 16:20:39 +0700

    rapi rapi

--- src/App.js
+++ src/App.js
@@ -9,13 +9,23 @@ import './App.css';
 import 'bootstrap'
 import('bootstrap/dist/css/bootstrap.css')
 
-/* eslint-disable */
-
 function App () {
+  const description = "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus."
+  const recents = [1, 2, 3, 4]
+  const suggestions = [1, 2, 3]
+  const emoticons = [
+    { icon: '😄', text: 'laugh' },
+    { icon: '👕', text: 'cloth' },
+    { icon: '😀', text: 'smile' },
+    { icon: '🐑', text: 'sheep' },
+    { icon: '✨', text: 'star' },
+    { icon: '👍', text: 'jempol' },
+  ]
+
   return (
     <div>
       <nav className="navbar navbar-expand-md fixed-top navbar-dark bg-dark">
-        <a href="#" className="navbar-brand">Rori React</a>
+        <span className="navbar-brand">Rori React</span>
         <button className="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -23,23 +33,23 @@ function App () {
         <div className="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="#">Dashboard <span className="sr-only">(current)</span></a>
+              <span className="nav-link">Dashboard <span className="sr-only">(current)</span></span>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Notifications</a>
+              <span className="nav-link">Notifications</span>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Profile</a>
+              <span className="nav-link">Profile</span>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Switch account</a>
+              <span className="nav-link">Switch account</span>
             </li>
             <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Settings</a>
+              <span className="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Settings</span>
               <div className="dropdown-menu" aria-labelledby="dropdown01">
-                <a className="dropdown-item" href="#">Action</a>
-                <a className="dropdown-item" href="#">Another action</a>
-                <a className="dropdown-item" href="#">Something else here</a>
+                <span className="dropdown-item">Action</span>
+                <span className="dropdown-item">Another action</span>
+                <span className="dropdown-item">Something else here</span>
               </div>
             </li>
           </ul>
@@ -51,21 +61,26 @@ function App () {
       </nav>
 
       <div className="nav-scroller bg-white box-shadow">
+        {/* eslint-disable-next-line jsx-a11y/no-distracting-elements */}
         <marquee>
           <nav className="nav nav-underline">
-            <span className="nav-link" role="img" aria-label="laugh">😄</span>
-            <span className="nav-link" role="img" aria-label="cloth">👕</span>
-            <span className="nav-link" role="img" aria-label="smile">😀</span>
-            <span className="nav-link" role="img" aria-label="sheep">🐑</span>
-            <span className="nav-link" role="img" aria-label="star">✨</span>
-            <span className="nav-link" role="img" aria-label="jempol">👍</span>
+          {
+            emoticons.map((value, e) => {
+              return <span
+                className="nav-link"
+                role="img"
+                aria-label="{value.text}"
+                key={e}>
+                { value.icon }
+              </span>
+            })
+          }
           </nav>
         </marquee>
       </div>
 
       <main role="main" className="container-fluid">
         <div className="d-flex align-items-center p-3 my-3 text-white-50 bg-purple rounded box-shadow">
-          {/* <img className="mr-3" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-outline.svg" alt="" width="48" height="48" /> */}
           <div className="lh-100">
             <h6 className="mb-0 text-white lh-100">Home</h6>
             <small>Since 2019</small>
@@ -74,83 +89,49 @@ function App () {
 
         <div className="my-3 p-3 bg-white rounded box-shadow">
           <h6 className="border-bottom border-gray pb-2 mb-0">Recent updates</h6>
-          <div className="media text-muted pt-3">
-            <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" className="mr-2 rounded" />
-            <p className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-              <strong className="d-block text-gray-dark">@username</strong>
-              Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-            </p>
-          </div>
-          <div className="media text-muted pt-3">
-            <img data-src="holder.js/32x32?theme=thumb&bg=e83e8c&fg=e83e8c&size=1" alt="" className="mr-2 rounded" />
-            <p className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-              <strong className="d-block text-gray-dark">@username</strong>
-              Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-            </p>
-          </div>
-          <div className="media text-muted pt-3">
-            <img data-src="holder.js/32x32?theme=thumb&bg=6f42c1&fg=6f42c1&size=1" alt="" className="mr-2 rounded" />
-            <p className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-              <strong className="d-block text-gray-dark">@username</strong>
-              Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-            </p>
-          </div>
+          {
+            recents.map((value, r) => {
+              return (
+                <div className="media text-muted pt-3" key={value}>
+                  <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" className="mr-2 rounded" />
+                  <p className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                    <strong className="d-block text-gray-dark">@username</strong>
+                    { description }
+                  </p>
+                </div>
+              )
+            })
+          }
           <small className="d-block text-right mt-3">
-            <a href="#">All updates</a>
+            <span>All updates</span>
           </small>
         </div>
 
         <div className="my-3 p-3 bg-white rounded box-shadow">
           <h6 className="border-bottom border-gray pb-2 mb-0">Suggestions</h6>
-          <div className="media text-muted pt-3">
-            <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" className="mr-2 rounded" />
-            <div className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-              <div className="d-flex justify-content-between align-items-center w-100">
-                <strong className="text-gray-dark">Full Name</strong>
-                <a href="#">Follow</a>
-              </div>
-              <span className="d-block">@username</span>
-            </div>
-          </div>
-          <div className="media text-muted pt-3">
-            <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" className="mr-2 rounded" />
-            <div className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-              <div className="d-flex justify-content-between align-items-center w-100">
-                <strong className="text-gray-dark">Full Name</strong>
-                <a href="#">Follow</a>
-              </div>
-              <span className="d-block">@username</span>
-            </div>
-          </div>
-          <div className="media text-muted pt-3">
-            <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" className="mr-2 rounded" />
-            <div className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-              <div className="d-flex justify-content-between align-items-center w-100">
-                <strong className="text-gray-dark">Full Name</strong>
-                <a href="#">Follow</a>
-              </div>
-              <span className="d-block">@username</span>
-            </div>
-          </div>
+          {
+            suggestions.map((value, s) => {
+              return (
+                <div className="media text-muted pt-3" key={value}>
+                  <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" className="mr-2 rounded" />
+                  <div className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                    <div className="d-flex justify-content-between align-items-center w-100">
+                      <strong className="text-gray-dark">Full Name</strong>
+                      <span>Follow</span>
+                    </div>
+                    <span className="d-block">@username</span>
+                  </div>
+                </div>
+              )
+            })
+          }
           <small className="d-block text-right mt-3">
-            <a href="#">All suggestions</a>
+            <span>All suggestions</span>
           </small>
         </div>
       </main>
     </div>
   );
 }
-
-// function Home() {
-//   return <h2>Home</h2>;
-// }
-
-// function About() {
-//   return <h2>About</h2>;
-// }
-
-// function Users() {
-//   return <h2>Users</h2>;
-// }
 
 export default App;
